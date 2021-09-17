75ca6aa7d6e7106a89577fc064a0f8d62567a039
U-BSD\476791 <476791@SHS-10D9C01D2.beaverton.k12.or.us>
2021-09-17 09:01:49 -0700

    Ensures input is between 1 and 100

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -5,10 +5,15 @@ using namespace std;
 int main()
 {
   int input = 0;
-  int number = 0;
+  int number = 25;
   cout << "Welcome to Guessing Game!" << endl
-       << "Guess the number between 1 and 100.";
+       << "Guess the number between 1 and 100." << endl;
   cin >> input;
+  while(input > 100 || input < 1)
+    {
+      cout << "Number is between 1 and 100 (inclusive)" << endl;
+      cin >> input;
+    }
   cout << input;
   return 0;
 }
---
+++ guessingGame.cpp~
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int input = 0;
+  int number = 0;
+  cout << "Welcome to Guessing Game!" << endl
+       << "Guess the number between 1 and 100.";
+  cin >> input;
+  cout << input;
+  return 0;
+}
