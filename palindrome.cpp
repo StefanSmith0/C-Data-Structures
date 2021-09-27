84dfb0779e2ff079ebdfd8b3d7fbdfb0c05ecf85
U-BSD\476791 <476791@SHS-10D9C01D2.beaverton.k12.or.us>
2021-09-27 09:14:02 -0700

    gamig

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,9 +6,22 @@ using namespace std;
 int main()
 {
   char strin[80];
-
+  char strout[80];
   cin.get(strin, 80);
-  cout << strin << endl;
-
+  int count = 0;
+  
+  for(int i = 0; i < 80; i++) {
+    if(strin[i] > 64 && strin[i] < 91) {
+      strout[count] = strin[i];
+    }
+    else if(strin[i] > 96 && strin[i] < 123) {
+      strout[count] = strin[i];
+    }
+    else if(strin[i] == 32) {
+      count--;
+    }
+    count++;
+  }
+  cout << strout << endl;
   return 0;
 }
