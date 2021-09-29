e299ae87054124e488c60f2e1ee57f5916950c43
U-BSD\476791 <476791@SHS-10D9C01D2.beaverton.k12.or.us>
2021-09-29 09:14:19 -0700

    almost works

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,21 +7,42 @@ int main()
 {
   char strin[80];
   char strout[80];
+  char strback[80];
   cin.get(strin, 80);
   int count = 0;
+  int countcon = 0;
   
   for(int i = 0; i < 80; i++) {
+    strout[count] = 0;
+    
     if(strin[i] > 64 && strin[i] < 91) {
       strout[count] = strin[i];
+      strout[count] += 32;
     }
     else if(strin[i] > 96 && strin[i] < 123) {
       strout[count] = strin[i];
     }
-    else if(strin[i] == 32) {
+    else {
       count--;
     }
     count++;
   }
   cout << strout << endl;
+  countcon = count;
+  for(int i = 0; i < countcon; i++) {
+    strback[i] = strout[count];
+    count--;
+  }
+  strback[countcon + 1] = 0;
+  cout << strback << endl;
+  
+  if(strcmp(strout, strback) == 0) {
+    cout << "Palindrome" << endl;
+  }
+  else {
+    cout << "Not a palindrome" << endl;
+  }
+  
+  cout << strback << " Count: " << count << endl;
   return 0;
 }
