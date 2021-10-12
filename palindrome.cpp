8f1d53bfd5331e3cd7a49ebf6643f2e500862472
U-BSD\476791 <476791@SHS-10D9C01D2.beaverton.k12.or.us>
2021-10-12 08:50:27 -0700

    Finished Palindrome

--- palindrome.cpp
+++ palindrome.cpp
@@ -8,13 +8,15 @@ int main()
   char strin[80];
   char strout[80];
   char strback[80];
-  cin.get(strin, 80);
   int count = 0;
   int countcon = 0;
-  
-  for(int i = 0; i < 80; i++) {
+
+  //Get input
+  cin.get(strin, 80);
+  //Copy input to strout
+  for(int i = 0; i < strlen(strin) + 1; i++) {
     strout[count] = 0;
-    
+    //Set all uppercase chars to lowercase
     if(strin[i] > 64 && strin[i] < 91) {
       strout[count] = strin[i];
       strout[count] += 32;
@@ -22,27 +24,30 @@ int main()
     else if(strin[i] > 96 && strin[i] < 123) {
       strout[count] = strin[i];
     }
+    //Cut out all non-letter characters
     else {
       count--;
     }
     count++;
   }
   cout << strout << endl;
+  //Make room for terminating character
   countcon = count;
+  count--;
+  //Iterate backwards through strout, copy to strback
   for(int i = 0; i < countcon; i++) {
     strback[i] = strout[count];
     count--;
   }
-  strback[countcon + 1] = 0;
+  //Set the terminating character at the end of strback
+  strback[countcon] = 0;
   cout << strback << endl;
-  
+  //Check if strout = strback, return answer
   if(strcmp(strout, strback) == 0) {
-    cout << "Palindrome" << endl;
+    cout << ">> Palindrome <<" << endl;
   }
   else {
-    cout << "Not a palindrome" << endl;
+    cout << ">> Not a palindrome <<" << endl;
   }
-  
-  cout << strback << " Count: " << count << endl;
   return 0;
 }
